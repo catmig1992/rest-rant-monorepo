6adeaa230ede7078e235b98454dcc3e8f14ab377
Catherine Miguel <catmig1992@gmail.com>
2023-02-24 04:00:11 -0800

    add administrator login

--- backend/controllers/places.js
+++ backend/controllers/places.js
@@ -4,6 +4,11 @@ const db = require("../models");
 const { Place, Comment, User } = db;
 
 router.post("/", async (req, res) => {
+  if (req.currentUser?.role !== "admin") {
+    return res
+      .status(403)
+      .json({ message: "You are not allowed to add a place" });
+  }
   if (!req.body.pic) {
     req.body.pic = "http://placekitten.com/400/400";
   }
@@ -45,6 +50,11 @@ router.get("/:placeId", async (req, res) => {
 });
 
 router.put("/:placeId", async (req, res) => {
+  if (req.currentUser?.role !== "admin") {
+    return res
+      .status(403)
+      .json({ message: "You are not allowed to edit places" });
+  }
   let placeId = Number(req.params.placeId);
   if (isNaN(placeId)) {
     res.status(404).json({ message: `Invalid id "${placeId}"` });
@@ -65,6 +75,11 @@ router.put("/:placeId", async (req, res) => {
 });
 
 router.delete("/:placeId", async (req, res) => {
+  if (req.currentUser?.role !== "admin") {
+    return res
+      .status(403)
+      .json({ message: "You are not allowed to delete places" });
+  }
   let placeId = Number(req.params.placeId);
   if (isNaN(placeId)) {
     res.status(404).json({ message: `Invalid id "${placeId}"` });
@@ -95,15 +110,15 @@ router.post("/:placeId/comments", async (req, res) => {
   });
 
   if (!place) {
-    return res
+    res
       .status(404)
       .json({ message: `Could not find place with id "${placeId}"` });
   }
 
   if (!req.currentUser) {
-    return res.status(404).json({
-      message: `You must be logged in to leave a rand or rave.`,
-    });
+    return res
+      .status(404)
+      .json({ message: `You must be logged in to leave a rand or rave.` });
   }
 
   const comment = await Comment.create({
